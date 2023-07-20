2856f2540df5f698f5402d66cbf63b73382ce57f
nayem9b <razwanniam1@gmail.com>
2023-07-20 22:42:16 +0600

    layout added

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,11 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const ErrorPage = () => {
+  const router = useRouter();
+  setTimeout(() => {
+    router.push("/");
+  }, 5000);
   return (
     <div>
       <h1>You faced an eroor</h1>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Button } from "antd";
+import Link from "next/link";
+import { ReactElement } from "react";
+import RootLayout from "../../components/Layouts/RootLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
-      <Button type="primary">Primary Button</Button>
+      <Button type="primary">
+        <Link href="/news">News</Link>
+      </Button>
     </div>
   );
 }
+
+Home.getLayout = function getLayout(page: ReactElement) {
+  return <RootLayout>{page}</RootLayout>;
+};

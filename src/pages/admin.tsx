91b61b2efe49a7a63ad0a14969216cad10df2f7b
nayem9b <razwanniam1@gmail.com>
2023-08-20 20:34:02 +0600

    admin page edited

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -5,7 +5,7 @@ import RootLayout from "../../components/Layouts/RootLayout";
 const Admin = () => {
   return (
     <div>
-      <h1>This is admin page</h1>
+      <h1>admin page</h1>
     </div>
   );
 };
